315c5035dbdb946fe16a3b560572b5a37f8c0458
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-25 20:50:12 -0300

    Login method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,15 +5,36 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTAuth;
 
 class AuthController extends Controller
 {
+
+    public function login(Request $request)
+    {
+        // grab credentials from the request
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response()->json(compact('token'));
+
+    }
     public function register(Request $request)
     {
         $credentials = $request->only(['name', 'email', 'password']);
         $credentials['password'] = bcrypt($credentials['password']);
 
         $user = User::create($credentials);
+
         return $user;
     }
 }
