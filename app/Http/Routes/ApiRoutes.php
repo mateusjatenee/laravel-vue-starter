4302931773bd8a7fc019a5c570333dc082967bca
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-25 20:50:29 -0300

    new route

--- app/Http/Routes/ApiRoutes.php
+++ app/Http/Routes/ApiRoutes.php
@@ -2,4 +2,5 @@
 
 Route::group(['middleware' => 'api', 'prefix' => 'api', 'namespace' => 'Api'], function () {
     Route::post('auth/register', 'AuthController@register');
+    Route::post('auth/login', 'AuthController@login');
 });
