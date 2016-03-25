9c77ef706dd6efcfd4992e05766066ad523f78e0
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-25 20:36:10 -0300

    register method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $credentials = $request->only(['name', 'email', 'password']);
+        $credentials['password'] = bcrypt($credentials['password']);
+
+        $user = User::create($credentials);
+        return $user;
+    }
 }
