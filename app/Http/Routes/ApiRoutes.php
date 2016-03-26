e0a81a470962bb7b3f7993752b271e2b91cc0446
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-25 21:13:11 -0300

    fixes routes

--- app/Http/Routes/ApiRoutes.php
+++ app/Http/Routes/ApiRoutes.php
@@ -1,6 +1,4 @@
 <?php
 
-Route::group(['middleware' => 'api', 'prefix' => 'api', 'namespace' => 'Api'], function () {
-    Route::post('auth/register', 'AuthController@register');
-    Route::post('auth/login', 'AuthController@login');
-});
+Route::post('auth/register', 'AuthController@register');
+Route::post('auth/login', 'AuthController@login');
