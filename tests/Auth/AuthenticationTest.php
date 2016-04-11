5611d513ab7a267d9a7dcedf4f84b5a46f485039
mateusjatenee <mateus.jatene@gmail.com>
2016-04-11 00:00:08 -0400

    Applied fixes from StyleCI

--- tests/Auth/AuthenticationTest.php
+++ tests/Auth/AuthenticationTest.php
@@ -7,6 +7,7 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class AuthenticationTest extends TestCase
 {
     use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
@@ -20,13 +21,13 @@ class AuthenticationTest extends TestCase
 
         $this->post('/api/auth/register', $credentials)
             ->seeJson([
-                'name' => $credentials['name'],
+                'name'  => $credentials['name'],
                 'email' => $credentials['email'],
             ])->assertResponseStatus(200);
     }
+
     public function test_user_can_login()
     {
-
         $password = str_random(10);
         $user = factory(User::class)->create([
             'password' => bcrypt($password),
@@ -35,7 +36,7 @@ class AuthenticationTest extends TestCase
         $token = JWTAuth::fromUser($user);
 
         $req = $this->post('/api/auth/login', [
-            'email' => $user->email,
+            'email'    => $user->email,
             'password' => $password,
         ])->seeJson([
             'token' => $token,
